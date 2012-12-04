2ded75628ee93bf92ea195a423fb3d6cc323755b
Aleksey Berezan <aleksey.berezan@gmail.com>
2012-12-04 19:00:42 +0200

    Put draft solution in ruby into correct file. | baterries_loading.rb Deleted redundand files.                      | gamble_poc.rb, sample.rb

--- baterries_loading.rb
+++ baterries_loading.rb
@@ -1,22 +1,65 @@
-def checkio(stones)
-	left = [0, []]
-	right = [0, []]
-	sorted = stones.sort()
-	while(sorted.length() > 0)
-		hand = left[0] < right[0] ? left : right
-		stone = sorted.pop()
-		hand[0] += stone
-		hand[1] << stone
-	end
-
-	# println left
-	# println right
-	return (left[0] - right[0]).abs
+def println(s)
+	print "#{s}\n"
 end
 
-# util methods
-def println(message)
-	print "#{message}\n"
+def copy(list)
+	a_copy = []
+	list.each { |item| a_copy << item }
+	return a_copy
+end
+
+def sum(list)
+	a_sum = 0
+	list.each { |item| a_sum += item }
+	return a_sum
+end
+
+def copy_with_adding(list, item_to_add)
+	return copy(list) << item_to_add
+end
+
+def copy_with_removal_at(list, removal_index)
+	result_list = copy(list)
+	result_list.delete_at(removal_index)
+	return result_list
+end
+
+def to_result_value(left_list, right_list)
+	dif = [(sum(left_list) - sum(right_list)).abs]
+	hands = [left_list.sort(), right_list.sort()]
+				.sort { |a, b| a.length <=> b.length }
+	return hands << dif
+end
+
+def store(result_hash, left_list, right_list)
+	value = to_result_value(left_list, right_list)
+	key = value.to_s()
+	result_hash[value.to_s()] = value
+end
+
+def gamble(result_hash, current_list, remaining_list)
+	store(result_hash, current_list, remaining_list)
+	if(remaining_list.length == 0)
+		return
+	end
+	i = 0
+	for item in remaining_list
+		gamble(result_hash, copy_with_adding(current_list, item), copy_with_removal_at(remaining_list, i))
+		i += 1
+	end
+end
+
+def value(item)
+	return item[2][0]
+end
+ 
+def checkio(remaining_list) 
+	result_hash = Hash.new() 
+	gamble(result_hash, [], remaining_list)
+	sorted = []
+	result_hash.each { |key, value| sorted << value }
+	sorted = sorted.sort { |a, b| value(a) <=> value(b) }
+	return value(sorted[0])
 end
 
 def assert(condition, message)
@@ -26,7 +69,7 @@ def assert(condition, message)
 	end
 	println "FAIL - #{message}"
 end
-
+  
 # assertions
 assert(checkio([10, 10]) == 0,  "First,  with equal weights")
 assert(checkio([10]) == 10,  "Second,  with single detail")
